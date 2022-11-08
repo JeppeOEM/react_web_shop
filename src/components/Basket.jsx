26afc6a1d88abd763219430dc16bd29a4c7ff523
Jeppe <jepp026b@edu.zealand.dk>
2022-11-08 16:34:43 +0100

    Basket can add prices

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,13 +1,19 @@
 function Basket(props) {
+  function getTotal() {
+    let total = 0;
+    props.cart.forEach((item) => {
+      total += item.amount * item.price;
+    });
+    return total;
+  }
   return (
     <section className="Basket">
       <ul>
         {props.cart.map((item) => (
           <li>
-            {item.productdisplayname}x {item.amount} {item.amount * item.price}
+            {item.productdisplayname}x {item.amount}, {item.amount * item.price},-
           </li>
         ))}
-        <li>Some Product x 3 123-</li>
       </ul>
       <button>Buy now</button>
     </section>
